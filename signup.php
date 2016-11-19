6216f051e0477c20a46fbf2769849583f9f2c7bb
ConradCarpenter <conccarp@yahoo.com>
2016-11-19 13:35:16 -0500

    inputs

--- signup.php
+++ signup.php
@@ -27,6 +27,7 @@
 			<input id = "name" name = "username" placeholder = "username" type = "text"> <br/><br/>
 			<label>Password :</label>
 			<input id = "password" name = "password" placeholder = "**********" type = "password"> <br/> <br/>
+			<label>ReEnter Password :</label>
 			<input id="passwordCheck" name = "passwordCheck" placeholder = "**********" type = "passwordCheck"> <br/> <br/>
 			<input name = "submit" type = "submit" value = " signUp ">
 
