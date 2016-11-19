f80ee32eeb26fed6169dcd5d4d5d578739196690
500ghz <itsnotomar@gmail.com>
2016-11-19 13:34:08 -0500

    fixed some spacing

--- index.php
+++ index.php
@@ -1,43 +1,44 @@
 <?php
 
-	include('login.php'); 
+include('login.php'); 
 
-	if(isset($_SESSION['login_user'])){
-		header("location: home.php");
-	}
+if(isset($_SESSION['login_user'])){
+	header("location: home.php");
+}
 
 ?>
 
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<title>Warzone Login</title>
+<head>
+	<title>Warzone Login</title>
 	
-	</head>
-	
-	<body>
-		<div id = "main">
-		<h1>Welcome to the Warzone!!!</h1>
-		
-		<div id = "login">
-		
-		<form action = "" method = "post">
-		
-			<label>Username :</label>
-			<input id = "name" name = "username" placeholder = "username" type = "text"> <br/><br/>
-			<label>Password :</label>
-			
-			<input id = "password" name = "password" placeholder = "**********" type = "password"> <br/> <br/>
-			<input name = "submit" type = "submit" value = " Login ">
+</head>
 
-			<span>
-				<?php 
-					echo $error; 
-				?>
-			</span>
-		</form>
-		</div>
+<body>
+	<div id = "main">
+		<h1>Welcome to the Warzone!!!</h1>
+		<h2><a href = "signup.php">Signup</a>
+			
+			<div id = "login">
+				
+				<form action = "" method = "post">
+					
+					<label>Username :</label>
+					<input id = "name" name = "username" placeholder = "username" type = "text"> <br/><br/>
+					<label>Password :</label>
+					
+					<input id = "password" name = "password" placeholder = "**********" type = "password"> <br/> <br/>
+					<input name = "submit" type = "submit" value = " Login ">
+
+					<span>
+						<?php 
+						echo $error; 
+						?>
+					</span>
+				</form>
+			</div>
 		</div>
 	</body>
-</html>
+	</html>
