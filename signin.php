06cfa0f05b6aa2744b4c85101598251d646a468b
ConradCarpenter <conccarp@yahoo.com>
2016-11-19 13:12:53 -0500

    Signin

--- signin.php
+++ signin.php
@@ -10,4 +10,16 @@ if (isset($_POST['submit'])) {
 		//Check if passwords are the same	
 		if($password != $passwordCheck) $error = "Passwords are not the same";
 		}
+		
+		$connection = mysql_connect("localhost", "root", "root");
+		$db = mysql_select_db("warzone", $connection);
+		
+		$query = mysql_query("select * from login where username='$username'", $connection);
+		$rows = mysql_num_rows($query);
+		
+		if($row > 0) $error = "Username is taken";
+		else{
+			$q = mysql_query("insert into login (username, password) values ('$username', '$password')", $connection);
+			header("Location: index.php"); 
+		}
 ?>
