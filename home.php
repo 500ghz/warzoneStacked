6a53906acf7a749031fe4eacd1cae863f097be3d
500ghz <itsnotomar@gmail.com>
2016-11-21 14:39:43 -0500

    attempting to get loggedin related things working

--- home.php
+++ home.php
@@ -1,42 +1,38 @@
 <?php
 
-	include('session.php');
-	
-	if (isset($_POST['btnLogin'])) {
-    authenticateUser($_POST['txtUsername'], $_POST['txtPassword']);
+include('session.php');
+
+if (isset($_POST['btnLogin'])) {
+	authenticateUser($_POST['txtUsername'], $_POST['txtPassword']);
 }
-	
+$current_time = time();
+echo $current_time;
+
 ?>
 
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Warzone Home</title>
-	</head>
-	
-	<body>
-		<div id = "profile">
+<head>
+	<title>Warzone Home</title>
+</head>
+
+<body>
+	<div id = "profile">
 		<b id = "welcome"> Welcome to the Warzone!!!! </b>
 		
 		<div id = "Userlogged_in">
-		
-		
-		<?php 
-		echo $login_session;
-		?>
-		
+
+			<?php 
+			echo $login_session;
+			?>
+
 		</div>
-		
-		
 		
 		<br/>
 		<form action = "logout.php" method = "post">
-		<button>Log Out</button>
+			<button>Log Out</button>
 		</form>
-		</div>
-		
-		
-	</body>
-	
+	</div>
+</body>
 </html>
--- login.php
+++ login.php
@@ -1,67 +1,96 @@
 <?php
 
-	// Starting Session
-	session_start(); 
-	// In case of an error, user this var
-	$error=''; 
-	
-	if (isset($_POST['submit'])) {
-		if (empty($_POST['username']) || empty($_POST['password'])) {
+// Starting Session
+session_start();
+// In case of an error, use this var
+$error='';
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect("localhost", "root", "root");
+
+// To protect MySQL injection for Security purpose
+$username = stripslashes($username);
+$password = stripslashes($password);
+
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+
+// Selecting Database
+$db = mysql_select_db("warzone", $connection);
+
+//echo isLoginSessionExpired();
+//echo loggedIn();
+//console.log($loggedin);
+
+function debug_to_console( $data ) {
+
+    if ( is_array( $data ) )
+        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    else
+        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+    echo $output;
+}
+
+//$loggedin = mysql_query("SELECT * FROM login where password='$password' AND username='$username' AND loggedin_time = !NULL", $connection);
+
+//debug_to_console($loggedin);
+
+if (isset($_POST['submit'])) {
+	if (empty($_POST['username']) || empty($_POST['password'])) {
 		$error = "Username or Password is invalid";
-		}
+	}
 	else {
-	// Var $username and $password
-	$username=$_POST['username'];
-	$password=$_POST['password'];
-	
-	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-	$connection = mysql_connect("localhost", "root", "root");
 
-	// To protect MySQL injection for Security purpose
-	$username = stripslashes($username);
-	$password = stripslashes($password);
+		function isLoginSessionExpired() {
+			$login_duration = 1; 
+			$current_time = time();
+			if(loggedIn()) {
+				$loggedintime = mysql_query("SELECT loggedin_time FROM login where username='$username'", $connection);
+				if(((time() - $loggedintime) > $login_duration)){ 
+					return true; 
+				} 
+			}
+			return false;
+		}
+
+		function loggedIn(){
+			// SQL query to fetch information of registerd users and finds user match.
+			$loggedin = mysql_query("SELECT * FROM login where password='$password' AND username='$username' AND loggedin_time = !NULL", $connection);
+			$rows = mysql_num_rows($loggedin);
+
+			if ($rows == 1) {
+				return true;
+			} else {
+				return false;
+			}
+
+		}
+		// SQL query to fetch information of registerd users and finds user match.
+		$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
+		$rows = mysql_num_rows($query);
+
+		//Gets current time as int
+		$time = time();
 		
-	$username = mysql_real_escape_string($username);
-	$password = mysql_real_escape_string($password);
-	
-	// Selecting Database
-	$db = mysql_select_db("warzone", $connection);
-	
-	// SQL query to fetch information of registerd users and finds user match.
-	$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
-	$rows = mysql_num_rows($query);
-	
-	$query2 = mysql_query("insert into userState username='$username' AND loggedIn");
+		//Query that adds loggedinTime to users table
+		$query2 = mysql_query("UPDATE login SET loggedinTime = '$time' WHERE username ='$username'");
 
-		if ($rows == 1) {
+		if ($rows == 1 && isLoginSessionExpired()) {
 			// Initializing Session
 			$_SESSION['login_user']=$username; 
 			
 			// Redirecting To Other Page
-			header("location: home.php"); 
+			header("location: home.php");
 		} else {
 			$error = "Username or Password is invalid";
-			}
+		}
 			// Closing Connection
-		mysql_close($connection); 
+		mysql_close($connection);
 	}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+}
 ?>
