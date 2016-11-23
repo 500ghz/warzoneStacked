294c7356aedfb9ec096da52b9b04f8cbc7b05677
Nick <clark.f.nicholas@gmail.com>
2016-11-22 20:34:08 -0500

    conrad's changes

---
+++ conrad_signup.php
@@ -0,0 +1,43 @@
+<?php
+
+	include('sig.php'); 
+
+	if(isset($_SESSION['login_user'])){
+		header("location: home.php");
+	}
+
+?>
+
+
+<html>
+	<head>
+		<title>Warzone Signup</title>
+	
+	</head>
+	
+	<body>
+		<div id = "main">
+		<h1>Welcome to the Warzone!!!</h1>
+		
+		<div id = "login">
+		
+		<form action = "" method = "post">
+		
+			<label>Username :</label>
+			<input id = "name" name = "username" placeholder = "username" type = "text"> <br/><br/>
+			<label>Password :</label>
+			<input id = "password" name = "password" placeholder = "**********" type = "password"> <br/> <br/>
+			<input id="passwordCheck" name = "passwordCheck" placeholder = "**********" type = "passwordCheck"> <br/> <br/>
+			<input name = "submit" type = "submit" value = " signUp ">
+
+			<span>
+				<?php 
+					echo $error; 
+				?>
+			</span>
+		</form>
+		</div>
+		</div>
+	</body>
+	</body>
+</html>
