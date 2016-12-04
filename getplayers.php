4b76c4242c357cf9d97cc20dbf9a89077ed9705b
500ghz <itsnotomar@gmail.com>
2016-12-03 19:54:59 -0500

    jQuery Ajax things

--- getplayers.php
+++ getplayers.php
@@ -1,18 +1,33 @@
 <?php
-$connection = mysql_connect('localhost', 'root', ''); 
 
-mysql_select_db('warzone');
 
-$query = "SELECT * FROM login"; 
-$result = mysql_query($query);
 
-echo "<table>"; 
-
-while($row = mysql_fetch_array($result)){   
-//Creates a loop to loop through results
-echo "<tr><td>" . $row['username'] . "</tr></td>" ; 
+if(isset($_POST['action']) && !empty($_POST['action'])) {
+	//echo "working";
+	$action = $_POST['action'];
+	switch($action) {
+		case 'test' : test();break;
+		case 'blah' : blah();break;
+        // ...etc...
+	}
 }
 
+function test(){
+	$connection = mysql_connect('localhost', 'root', ''); 
+
+	mysql_select_db('warzone');
+
+	$query = "SELECT * FROM login"; 
+	$result = mysql_query($query);
+
+	echo "<table>"; 
+
+	while($row = mysql_fetch_array($result)){   
+	//Creates a loop to loop through results
+		echo "<tr><td>" . $row['username'] . "</td> <td><button>Invite this fucking guy</button></td> </tr>" ; 
+	}
+
 echo "</table>"; //Close the table in HTML
-		
+
+}
 ?>
--- home.php
+++ home.php
@@ -5,26 +5,40 @@ if (isset($_POST['btnLogin'])) {
 	authenticateUser($_POST['txtUsername'], $_POST['txtPassword']);
 }
 $session = session_id();
-echo $session;
 
 ?>
 
-
-<!DOCTYPE html>
 <html>
 <head>
 	<title>Warzone Home</title>
 </head>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script>
+function something(){
 
+	$.ajax({ url: 'getplayers.php',
+		data: {action: 'test'},
+		type: 'post',
+		success: function(output) {
+			console.log(output);
+			replaceDiv(output);
+		},
+		error: function(output) {
+			console.log(output);
+					//console.log("Details: " + desc + "\nError:" + err);
+				}
+			});
+}
+function replaceDiv(output){
+	document.getElementById("Omar").innerHTML = output;
+
+}
+</script>
 <body>
 	<div id = "profile">
 		<b id = "welcome"> Welcome to the Warzone!!!! </b>
 		
 		<div id = "Userlogged_in">
-
-			<?php 
-			echo $login_session;
-			?>
 
 		</div>
 		
@@ -32,6 +46,10 @@ echo $session;
 		<form action = "logout.php" method = "post">
 			<button>Log Out</button>
 		</form>
+		<a href = "getplayers.php">this</a>
+		<button onclick="something()">Button</button>
+		
 	</div>
+	<div id = "Omar">Content</div>
 </body>
 </html>
